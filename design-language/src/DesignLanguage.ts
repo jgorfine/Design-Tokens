a2ecc197c069977be261cfe7a8bcfcb3d2532555
Jen Gorfine <jennifer.gorfine@gmail.com>
2020-10-15 22:02:53 -0400

    Moved Colors to thier own file

--- design-language/src/DesignLanguage.ts
+++ design-language/src/DesignLanguage.ts
@@ -1,70 +1,20 @@
-import {Color, DropShadow, Image, Lottie, Toward, Typograph, Font, LinearGradient, Point2D, TextAlignment} from '@diez/prefabs';
+import {DropShadow, Image, LinearGradient, Lottie, Toward, Typograph, Font, Point2D, TextAlignment} from '@diez/prefabs';
+import Colors from './components/atoms/Colors';
 import {Margin} from './components/Margin';
 
 /**
  * You can collect anything inside a Diez component. Design tokens specified as
  * properties will be made available in the SDKs transpiled with Diez.
  */
-const colors = {
-  white: Color.hex('#FFF'),
-  black: Color.hex('#000'),
-  // Mobile Team Colors
-  // see: https://fiscalnote2.atlassian.net/wiki/spaces/MNBV/pages/1009221679/Mobile+Design+System
-  purplePrimary: Color.hex('#442547'),
-  taupeLight: Color.hex('#AD9C94'),
-  taupeMid: Color.hex('#826F75'),
-  taupeDark: Color.hex('#635257'),
-  tealLight: Color.hex('#E3F6F7'),
-  tealMid: Color.hex('#85C7CB'),
-  tealBright: Color.hex('#38ACB3'),
-  tealPrimary: Color.hex('#008991'),
-  tealDark: Color.hex('#037D85'),
-  tanBackground: Color.hex('#FBFEF3'),
-  tanText: Color.hex('#F3F7E8'),
-  tanLight: Color.hex('#F0F3E9'),
-  tanMid: Color.hex('#DADCD3'),
-  tanDark: Color.hex('#D2D1C5'),
-  greyLight: Color.hex('#D8D8D8'),
-  night: Color.hex('#131313'),
-  redSunset: Color.hex('#BF2909'),
-  redFire: Color.hex('#E0310B'),
-  orangeOptimistic: Color.hex('#FF6B2B'),
-  yellowBright: Color.hex('#F0C11C'),
-  yellowButtercup: Color.hex('#F6DC7E'),
-  partyRepublican: Color.hex('#BE2B2B'),
-  partyIndependent: Color.hex('#932079'),
-  partyOther: Color.hex('#635257'),
-  partyLibertarian: Color.hex('#865E00'),
-  partyDemocrat: Color.hex('#2270E5'),
-  partyGreen: Color.hex('#006F3A'),
-  spectrumFarRight: Color.hex('#AF0B0B'),
-  spectrumRight: Color.hex('#BC185D'),
-  spectrumMiddle: Color.hex('#7F3A86'),
-  spectrumLeft: Color.hex('#25AEFB'),
-  spectrumFarLeft: Color.hex('#2270E5'),
-  infoOrange: Color.hex('#F36DDD'),
-  infoErrorRed: Color.hex('#E82733'),
-  infoGoodGreen: Color.hex('#00914C'),
-  infoYellowGreen: Color.hex('#ADA94E'),
-  activeGreen: Color.hex('006F3A'),
-  // Marketing Team Colors
-  // see: https://drive.google.com/file/d/1k5LXQGbVHQVAyGOaktvUkYpL6WTDXkhO/view
-  purpleBold: Color.hex('#442547'),
-  redAlert: Color.hex('#E0310B'),
-  tealTech: Color.hex('#3AC6CD'),
-  tealGrassroot: Color.hex('#087D85'),
-  greyPower: Color.hex('#939598'),
-  greyLight: Color.hex('#F2F2F2'),
-}
 
 /**
  * You can reference properties from other components.
  */
 const palette = {
-  contentBackground: colors.white,
-  text: colors.black,
-  caption: colors.purple,
-  headerBackground: LinearGradient.make(Toward.Bottom, colors.darkPurple, colors.black),
+  contentBackground: Colors.greyLight,
+  text: Colors.tealTech,
+  caption: Colors.purplePrimary,
+  headerBackground: LinearGradient.make(Toward.Bottom, Colors.purplePrimary, Colors.black),
 }
 
 /**
@@ -140,7 +90,7 @@ const shadows = {
   logo: new DropShadow({
     offset: Point2D.make(0, 1),
     radius: 16,
-    color: colors.black.fade(0.59),
+    color: Colors.black.fade(0.59),
   }),
 }
 
